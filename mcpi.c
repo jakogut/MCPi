e16ce25ab25e10150735e61ed8ee5dad8c7b9747
Joseph Kogut <joseph.kogut@gmail.com>
2012-12-23 16:37:41 -0700

    Changed non threaded implementation of Monte Carlo pi generation to use sequential samples instead of random samples

--- mcpi.c
+++ mcpi.c
@@ -3,31 +3,66 @@
 #include <time.h>
 #include <math.h>
 
-#define BUFFER_SIZE 64000000
+#define BUFFER_SIZE_SQRT 4096
 
-float sample_buffer[BUFFER_SIZE][2];
-
-void randomize_buffer()
+float*** create_buffer()
 {
-	for(int i = 0; i < BUFFER_SIZE; i++)
-		for(int j = 0; j < 2; j++)
-			sample_buffer[i][j] = rand() / (float)RAND_MAX;
+	float*** buffer = (float***)malloc(sizeof(float**) * BUFFER_SIZE_SQRT);
+	for(int i = 0; i < BUFFER_SIZE_SQRT; i++)
+	{
+		buffer[i] = (float**)malloc(sizeof(float*) * BUFFER_SIZE_SQRT);
+
+		for(int j = 0; j < BUFFER_SIZE_SQRT; j++)
+			buffer[i][j] = (float*)malloc(sizeof(float) * 2);
+	}
+
+	return buffer;
 }
 
-float calculate_pi()
+void destroy_buffer(float*** buffer)
+{
+	for(int i = 0; i < BUFFER_SIZE_SQRT; i++)
+	{
+		for(int j = 0; j < BUFFER_SIZE_SQRT; j++)
+			free(buffer[i][j]);
+
+		free(buffer[i]);
+	}
+
+	free(buffer);
+}
+
+void fill_buffer(float*** buffer)
+{
+	float increment = 1.0 / (float)BUFFER_SIZE_SQRT;
+
+	for(int x = 0; x < BUFFER_SIZE_SQRT; x++)
+	{
+		for(int y = 0; y < BUFFER_SIZE_SQRT; y++)
+		{
+			buffer[x][y][0] = x * increment;
+			buffer[x][y][1] = y * increment;
+		}
+	}
+}
+
+float calculate_pi(float*** buffer)
 {
 	int hits = 0;
-	for(int i = 0; i < BUFFER_SIZE; i++)
-		if((powf(sample_buffer[i][0], 2) + powf(sample_buffer[i][1], 2)) < 1)
-			++hits;
+	for(int x = 0; x < BUFFER_SIZE_SQRT; x++)
+		for(int y = 0; y < BUFFER_SIZE_SQRT; y++)
+			if((powf(buffer[x][y][0], 2) + powf(buffer[x][y][1], 2)) < 1)
+				++hits;
 
-	return (hits * 4) / (float)((BUFFER_SIZE));
+	return (hits * 4) / (float)(BUFFER_SIZE_SQRT * BUFFER_SIZE_SQRT);
 }
 
 int main()
 {
-	srand(time(NULL));
+	float*** sample_buffer = create_buffer();
 
-	randomize_buffer();
-	printf("%f\n", calculate_pi());
+	fill_buffer(sample_buffer);
+	printf("%f\n", calculate_pi(sample_buffer));
+
+	destroy_buffer(sample_buffer);
 }
